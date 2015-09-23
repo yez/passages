19b8dd1397b28a0aa92df82b1618d81f61f9526e
Jake Yesbeck <jake@apartmentlist.com>
2015-09-22 22:49:09 -0700

    Use new Formatter

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,8 +1,14 @@
+require 'lib/formatter'
+
 module Roots
   class RoutesController < ActionController::Base
     layout false
 
     def routes
+      rails_routes = Rails.application.routes.routes
+      inspector = ActionDispatch::Routing::RoutesInspector.new(rails_routes)
+      formatter = Formatter.new
+      result = inspector.format(formatter)
     end
   end
 end
