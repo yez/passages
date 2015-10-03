e1802a141bfa1c9087a157a26b320f6fdfefec38
Jake Yesbeck <jake@apartmentlist.com>
2015-10-02 21:23:20 -0700

    Move roots.css to application.css and style the page more
    Remove button from search bar.
    
    Differentiate between Engine and Application routes.

--- lib/route_collection.rb
+++ lib/route_collection.rb
@@ -14,5 +14,27 @@ module Roots
     def each(&block)
       @routes.each(&block)
     end
+
+    def engine_routes
+      if @engine_routes.nil?
+        partition_routes
+      end
+
+      @engine_routes
+    end
+
+    def application_routes
+      if @application_routes.nil?
+        partition_routes
+      end
+
+      @application_routes
+    end
+
+    private
+
+    def partition_routes
+      @engine_routes, @application_routes = partition(&:engine?)
+    end
   end
 end
