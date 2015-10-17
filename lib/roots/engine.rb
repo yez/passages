3beeccabfbbcd8b41a9bf6f6541b81f1c8dd7261
Jake Yesbeck <jake@apartmentlist.com>
2015-10-16 17:07:30 -0700

    Allow Roots to mount itself

--- lib/roots/engine.rb
+++ lib/roots/engine.rb
@@ -1,5 +1,11 @@
 module Roots
   class Engine < ::Rails::Engine
      isolate_namespace Roots
+
+     initializer 'roots', before: :load_config_initializers do |app|
+      Rails.application.routes.append do
+        mount Roots::Engine, at: '/roots'
+      end
+    end
   end
 end
