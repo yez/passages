5f0232c5ced2800d41f438f3da60c13039bf8ecd
Jake Yesbeck <jake@apartmentlist.com>
2015-10-27 22:28:32 -0700

    Basic search and highlight functionality

--- app/assets/javascripts/roots/application.js
+++ app/assets/javascripts/roots/application.js
@@ -1,8 +1,22 @@
 $(document).on('ready', function(){
-
+  $('input[name=search]').keyup(function(event){
+    var searchTerm = event.target.value;
+    if(searchTerm.length > 0){
+      searchResults(searchTerm);
+    }
+  });
 });
 
+function clearBackground(){
+  $('td').css('background-color', 'white');
+}
 
 function searchResults(query) {
-
+  clearBackground();
+  var matches = {};
+  $.each($('[data-search*=' + query), function(index, value) {
+    $(value).css('background-color', 'yellow');
+  });
 }
+
+
