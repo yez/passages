ef49b3d469e1863a675f36615ed00e64bb655681
Jake Yesbeck <jake@apartmentlist.com>
2015-10-28 21:08:01 -0700

    Add matching section and update search functionality

--- app/assets/javascripts/roots/application.js
+++ app/assets/javascripts/roots/application.js
@@ -1,22 +1,32 @@
-$(document).on('ready', function(){
-  $('input[name=search]').keyup(function(event){
-    var searchTerm = event.target.value;
-    if(searchTerm.length > 0){
-      searchResults(searchTerm);
-    }
-  });
-});
-
-function clearBackground(){
-  $('td').css('background-color', 'white');
+function clearResultTable(){
+  $('table.matching-term').html('');
 }
 
 function searchResults(query) {
-  clearBackground();
-  var matches = {};
-  $.each($('[data-search*=' + query), function(index, value) {
-    $(value).css('background-color', 'yellow');
-  });
+  clearResultTable();
+  var found = $('[data-search*=' + query).parent('tr');
+  return found;
 }
 
+function addToResultTable(result){
+  var table = $('.matching-term');
+  var lastChild = table.find('tr:last');
+  if(lastChild.length == 0) {
+    table.html(result);
+  }
+  else {
+    lastChild.after(result);
+  }
+}
 
+$(document).on('ready', function(){
+  $('input[name=search]').keyup(function(event){
+    var searchTerm = event.target.value;
+    if(searchTerm.length > 0) {
+      var results = searchResults(searchTerm.toLowerCase());
+      $.each(results, function(index, value){
+        addToResultTable($(value).clone());
+      });
+    }
+  });
+});
