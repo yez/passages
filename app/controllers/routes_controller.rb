21dde15771cbd98c90451d69d83085730a1e9f1c
Jake Yesbeck <jake@apartmentlist.com>
2015-11-07 21:07:16 -0800

    Use new EngineRouteCollection in controller

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,23 +1,26 @@
 require 'lib/route_collection'
+require 'lib/engine_route_collection'
 
 module Roots
   class RoutesController < ActionController::Base
     layout false
 
     def routes
-      @route_collection = RouteCollection.new(app_routes: application_routes, eng_routes: engine_routes)
+      @routes = application_routes + engline_routes
     end
 
     private
 
     def engine_routes
-      ::Rails::Engine.subclasses.map do |engine|
-        { engine: engine.name, routes: [*engine.routes.routes.routes] }
-      end.flatten
+      RouteCollection.new(
+        ::Rails::Engine.subclasses.map do |engine|
+          { engine: engine.name, routes: [*engine.routes.routes.routes] }
+        end.flatten
+      )
     end
 
     def application_routes
-      [*Rails.application.routes.routes]
+      EngineRouteCollection.new([*Rails.application.routes.routes])
     end
   end
 end
