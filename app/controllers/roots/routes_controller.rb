10d22a5880c5c2049ff119c81bd85e17c859f68f
Jake Yesbeck <jake@apartmentlist.com>
2015-11-11 22:51:17 -0800

    Add back mount route

--- app/controllers/roots/routes_controller.rb
+++ app/controllers/roots/routes_controller.rb
@@ -1,3 +1,4 @@
+require 'lib/mount_route'
 require 'lib/route_collection'
 require 'lib/engine_route_collection'
 
@@ -6,21 +7,38 @@ module Roots
     layout false
 
     def routes
-      @routes = application_routes
       @engine_routes = engine_routes
+      @routes = application_routes
     end
 
     private
 
     def engine_routes
       ::Rails::Engine.subclasses.map do |engine|
-        { engine: engine.name, routes: EngineRouteCollection.new(engine.name, [*engine.routes.routes.routes]) }
-      end.flatten
+        _engine_routes = EngineRouteCollection.new(engine.name, [*engine.routes.routes.routes])
+        next if _engine_routes.all?(&:internal?)
+
+        { engine: engine.name, routes: _engine_routes }
+      end.compact
     end
 
     def application_routes
-      app_routes = Rails.application.routes.routes
+      mount_routes, app_routes = Rails.application.routes.routes.partition(&method(:mount_route?))
+
+      if mount_routes.present?
+        @mount_routes = {}
+        mount_routes.each do |route|
+          mounted = MountRoute.new(route)
+          @mount_routes[mounted.engine_name] = mounted unless mounted.internal?
+        end
+      end
+
       RouteCollection.new([*app_routes])
+    end
+
+    def mount_route?(route)
+      app_class = route.try(:app).try(:app)
+      app_class.is_a?(Class) && app_class.ancestors.include?(Rails::Engine)
     end
   end
 end
---
+++ lib/mount_route.rb
@@ -0,0 +1,19 @@
+class MountRoute
+  def initialize(route)
+    @route = route
+    @wrapped = ActionDispatch::Routing::RouteWrapper.new(route)
+  end
+
+  def engine_name
+    @route.app.app.name
+  end
+
+  def internal?
+    @wrapped.internal?
+  end
+
+  def path
+    @wrapped.path
+  end
+end
+
