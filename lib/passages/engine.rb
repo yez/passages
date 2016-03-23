581c77eb8096f1c55bf3ee6b404987b5a1a28730
Jake Yesbeck <jake@apartmentlist.com>
2016-03-23 13:51:55 -0700

    Support precompilation for assets

--- lib/passages/engine.rb
+++ lib/passages/engine.rb
@@ -2,6 +2,10 @@ module Passages
   class Engine < ::Rails::Engine
     isolate_namespace Passages
 
+    initializer "passages.assets.precompile" do |app|
+      app.config.assets.precompile += %w(application.css application.js)
+    end
+
     initializer 'passages', after: :load_config_initializers do |app|
       if Passages.config.automount
         Rails.application.routes.prepend do
